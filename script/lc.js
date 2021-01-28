e747921412e92483ec91fe3acc1f9477dade36f4
anneiric <69729163+anneiric@users.noreply.github.com>
2021-01-28 10:34:57 +0000

    updated lc.js

--- script/lc.js
+++ script/lc.js
@@ -1,20 +1,17 @@
-;(function () {
-    var english = document.getElementById("english")
-    var el = document.getElementById("display")
-    var maths = document.getElementById("maths")
-    var irish = document.getElementById("irish")
-    var subject4 = document.getElementById("subject4")
-    var subject5 = document.getElementById("subject5")
-    var subject6 = document.getElementById("subject6")
+var english = document.getElementById("english")
+var el = document.getElementById("display")
+var maths = document.getElementById("maths")
+var irish = document.getElementById("irish")
+var subject4 = document.getElementById("subject4")
+var subject5 = document.getElementById("subject5")
+var subject6 = document.getElementById("subject6")
   
-    document.getElementById("showVal").onclick = function () {
-      el.value =
-        +maths.value +
-        +english.value +
-        +irish.value +
-        +subject4.value +
-        +subject5.value +
-        +subject6.value
-    }
-  })()
-  
+document.getElementById("showVal").onclick = function () {
+  el.value =
+    +maths.value +
+    +english.value +
+    +irish.value +
+    +subject4.value +
+    +subject5.value +
+    +subject6.value
+}
