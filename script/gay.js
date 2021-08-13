e61ca17ffa483c7e401aa777c9e064c229798889
anneiric <69729163+anneiric@users.noreply.github.com>
2021-08-13 22:42:20 +0100

    text on gay <3

--- script/gay.js
+++ script/gay.js
@@ -1,5 +1,11 @@
+var text = [
+  "0% </br> go take a shower",
+  "30% </br> stream arca to be gayer",
+  "50% </br> let's be besties",
+  "80% </br> can we kiss on the pride anti-homeless bench",
+  "100% </br> how does it feel like being hot",
+]
 function result() {
-    document.getElementById("display").innerHTML =
-      "0% </br> go take a shower"
-  }
-  
+  document.getElementById("display").innerHTML =
+    text[Math.floor(Math.random() * text.length)]
+}
